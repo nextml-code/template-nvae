Fix image size in model. Fix image scaling in repr

mnist_nvae/architecture/model.py
@@ -17,7 +17,7 @@ class Model(nn.Module):
 
         self.latent_channels = 20
         self.decoder = architecture.DecoderNVAE(
-            example_features=self.encoder(torch.zeros(1, 16, 16)),
+            example_features=self.encoder(torch.zeros(1, 28, 28)),
             latent_channels=self.latent_channels,
         )
 

mnist_nvae/architecture/prediction.py
@@ -31,7 +31,7 @@ class Prediction(BaseModel):
 
     def image(self):
         return Image.fromarray(np.uint8(
-            (self.predicted_image.squeeze(0).cpu().numpy() + 1) / 2 * 255
+            self.predicted_image.squeeze(0).cpu().numpy()
         ))
 
     def representation(self):
@@ -72,7 +72,7 @@ class PredictionBatch(BaseModel):
     def loss(self, examples):
         return (
             self.mse(examples)
-            + 0.01 * sum(self.kl_losses)
+            + sum(self.kl_losses)
         )
 
     def mse(self, examples):